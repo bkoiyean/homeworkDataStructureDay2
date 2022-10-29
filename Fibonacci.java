cb3da97df99456dfb86d012254502d828791e4c3
bkoiyean <115851631+bkoiyean@users.noreply.github.com>
2022-10-30 08:55:43 +1100

    Add files via upload

--- Fibonacci.java
+++ Fibonacci.java
@@ -16,6 +16,18 @@ public class Fibonacci {
 		}
 
 		System.out.println("Fibonacci of number " + num + " is: " + current);
+		System.out.println("Fibonacci of number " + num + " is: " + fibo(num));
+
+	}
+
+	public static int fibo(int num) {
+		if (num == 0) {
+			return 0;
+		} else if (num == 1) {
+			return 1;
+		} else {
+			return fibo(num - 1) + fibo(num - 2);
+		}
 	}
 
 }
