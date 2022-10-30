182ee1f4b9496a7c29eb13c1cc9bd9b516cc3179
bkoiyean <115851631+bkoiyean@users.noreply.github.com>
2022-10-31 00:27:27 +1100

    Add files via upload

--- Fibonacci.java
+++ Fibonacci.java
@@ -17,6 +17,7 @@ public class Fibonacci {
 
 		System.out.println("Fibonacci of number " + num + " is: " + current);
 		System.out.println("Fibonacci of number " + num + " is: " + fibo(num));
+		System.out.println("Fibonacci of number " + num + " is: " + binaryFibo(num));
 
 	}
 
@@ -27,6 +28,24 @@ public class Fibonacci {
 			return 1;
 		} else {
 			return fibo(num - 1) + fibo(num - 2);
+		}
+	}
+	
+	public static int binaryFibo(int num) {
+		int temp;
+		if (num == 0) 
+			return 0;
+		else if (num == 1) 
+			return 1;
+		else if (num == 2) 
+			return 1;
+		else if (num % 2 == 0) {
+			temp = num/2;
+			return 2*binaryFibo(temp)*binaryFibo(temp + 1) - binaryFibo(temp)*binaryFibo(temp);
+		}
+		else {
+			temp = (num - 1)/2;
+			return binaryFibo(temp)*binaryFibo(temp) + binaryFibo(temp+1)*binaryFibo(temp+1);
 		}
 	}
 
